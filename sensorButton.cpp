2aa0d843ceeedcd05eb878d27f31d7e0d3924ec0
João Adriano Freitas <jaafreitas@gmail.com>
2019-06-29 22:56:57 -0300

    Box open/closed status and alarm system.

--- sensorButton.cpp
+++ sensorButton.cpp
@@ -1,33 +1,49 @@
 #include "sensorButton.h"
 #include "settings.h"
 
+unsigned long lastDoorRead = 0;
+unsigned long lastDoorAlarm = 0;
+bool doorOpenLastStatus = false;
+
+unsigned long lastBoxRead = 0;
+unsigned long lastBoxAlarm = 0;
+bool doorBoxLastStatus = false;
+
 void setupSensorButton() {
   pinMode(SENSOR_DOOR_PIN, INPUT_PULLUP);
+  pinMode(SENSOR_BOX_PIN, INPUT_PULLUP);
 }
 
-
-void loopSensorButton(Conn* conn) {
-  bool doorOpen = digitalRead(SENSOR_DOOR_PIN);
+void loopSensorButton(Conn* conn, uint8_t pin, String sensor, 
+  unsigned long &lastSensorRead, unsigned long &lastSensorAlarm, bool &sensorOpenLastStatus,
+  unsigned long sensorNotifyInterval, unsigned long sensorAlarmNotifyAfter) {
+      
+  bool sensorOpen = digitalRead(pin);
   
-  static unsigned long lastRead = 0;
-  static unsigned long lastAlarm = 0;
-  static bool doorOpenLastStatus = false;
-  
-  // The door has just been closed.
-  if (doorOpenLastStatus and !doorOpen) {
-    conn->notify_sensor("door", "closed", false);
-    delay(10);
+  // The sensor has just been closed.
+  if (sensorOpenLastStatus and !sensorOpen) {
+    conn->notify_sensor(sensor, SENSOR_STATUS_CLOSED, false);
+    // "blink without delay"
+    delay(50);
   }
   
-  doorOpenLastStatus = doorOpen;
-  if (doorOpen) {
-    if (millis() - lastRead > SENSOR_DOOR_NOTIFY_INTERVAL) {
-      lastRead = millis();
-      bool alarm = millis() - lastAlarm > SENSOR_DOOR_ALARM_NOTIFY_AFTER;
-      conn->notify_sensor("door", "open", alarm);
+  sensorOpenLastStatus = sensorOpen;
+  if (sensorOpen) {
+    if (millis() - lastSensorRead > sensorNotifyInterval) {
+      lastSensorRead = millis();
+      bool alarm = millis() - lastSensorAlarm >= sensorAlarmNotifyAfter;
+      conn->notify_sensor(sensor, SENSOR_STATUS_OPEN, alarm);
     }
     
   } else {
-    lastAlarm = millis();
+    lastSensorAlarm = millis();
   }
+}
+
+void loopSensorButton(Conn* conn) {
+  loopSensorButton(conn, SENSOR_DOOR_PIN, SENSOR_DOOR_NAME, lastDoorRead, lastDoorAlarm, doorOpenLastStatus, 
+    SENSOR_DOOR_NOTIFY_INTERVAL, SENSOR_DOOR_ALARM_NOTIFY_AFTER);
+    
+  loopSensorButton(conn, SENSOR_BOX_PIN, SENSOR_BOX_NAME, lastBoxRead, lastBoxAlarm, doorBoxLastStatus, 
+    SENSOR_BOX_NOTIFY_INTERVAL, SENSOR_BOX_ALARM_NOTIFY_AFTER);
 }
--- settings.h
+++ settings.h
@@ -9,7 +9,7 @@
 #define WIFI_SSID "WIFI_SSID"
 #define WIFI_PASSWORD "WIFI_PASSWORD"
 
-#define MQTT_CONNECTION_RETRY 10000
+#define MQTT_CONNECTION_RETRY 60000
 
 #define MQTT_SERVER "MQTT_SERVER"
 #define MQTT_PORT 1883
@@ -20,8 +20,17 @@
 #define NTP_INTERVAL 1 * 60 * 60 * 1000 // Every 1 hour (in milliseconds)
 #define TZ_OFFSET (-3 * 60 * 60) // in seconds.
 
+#define SENSOR_STATUS_OPEN "open"
+#define SENSOR_STATUS_CLOSED "closed"
+
+#define SENSOR_DOOR_NAME "door"
 #define SENSOR_DOOR_PIN D1
 #define SENSOR_DOOR_NOTIFY_INTERVAL 1000 // Every second.
-#define SENSOR_DOOR_ALARM_NOTIFY 15000
+#define SENSOR_DOOR_ALARM_NOTIFY_AFTER 10000
+
+#define SENSOR_BOX_NAME "box"
+#define SENSOR_BOX_PIN D2
+#define SENSOR_BOX_NOTIFY_INTERVAL 1000 // Every second.
+#define SENSOR_BOX_ALARM_NOTIFY_AFTER 0
 
 #define ALARM_PIN D4
