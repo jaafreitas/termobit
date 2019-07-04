c1ccb9fe36ae41be0bec5423c9fe0dce0feb93b9
João Adriano Freitas <jaafreitas@gmail.com>
2019-07-04 20:57:05 -0300

    Using consts instead of #defines

--- settings.h
+++ settings.h
@@ -4,58 +4,58 @@
 
 #define DEBUG
 
-#define HOSTNAME_PREFIX "termobit"
+static const char* HOSTNAME_PREFIX = "termobit";
 
-#define WIFI_SSID "WIFI_SSID"
-#define WIFI_PASSWORD "WIFI_PASSWORD"
-#define WIFI_CONNECTION_RETRIES 20
+static const char* WIFI_SSID = "WIFI_SSID";
+static const char* WIFI_PASSWORD = "WIFI_PASSWORD";
+static const uint8_t WIFI_CONNECTION_RETRIES = 20;
 
-#define MQTT_CONNECTION_RETRY 60000
+static const unsigned long MQTT_CONNECTION_RETRY = 60000;
 
-#define MQTT_SERVER "MQTT_SERVER"
-#define MQTT_PORT 1883
+static const char* MQTT_SERVER = "MQTT_SERVER";
+static const uint16_t MQTT_PORT = 1883;
 
-#define NTP_SERVER "NTP_SERVER"
-#define NTP_PORT 123
-#define NTP_LOCALPORT 2390
-#define NTP_INTERVAL 1 * 60 * 60 * 1000 // Every 1 hour (in milliseconds)
-#define TZ_OFFSET (-3 * 60 * 60) // in seconds.
+static const char* NTP_SERVER = "NTP_SERVER";
+static const uint16_t NTP_PORT = 123;
+static const uint16_t NTP_LOCALPORT = 2390;
+static const unsigned long NTP_INTERVAL = 1 * 60 * 60 * 1000; // Every 1 hour (in milliseconds)
+static const unsigned long TZ_OFFSET = (-3 * 60 * 60); // in seconds.
 
-#define SENSOR_STATUS_OPEN "open"
-#define SENSOR_STATUS_CLOSED "closed"
+static const char* SENSOR_STATUS_OPEN = "open";
+static const char* SENSOR_STATUS_CLOSED = "closed";
 
-#define SENSOR_TEMPERATURE "temperature"
-#define SENSOR_TEMPERATURE_ALARM_LOW 2.0
-#define SENSOR_TEMPERATURE_ALARM_HIGH 6.0
-#define SENSOR_TEMPERATURE_VARIATION_PERCENTAGE 10
+static const char* SENSOR_TEMPERATURE = "temperature";
+static const float SENSOR_TEMPERATURE_ALARM_LOW = 2.0;
+static const float SENSOR_TEMPERATURE_ALARM_HIGH = 6.0;
+static const float SENSOR_TEMPERATURE_VARIATION_PERCENTAGE = 10;
 
-#define SENSOR_DOOR_NAME "door"
-#define SENSOR_DOOR_PIN D1
-#define SENSOR_DOOR_NOTIFY_INTERVAL 1000 // Every second.
-#define SENSOR_DOOR_ALARM_NOTIFY_AFTER 10000
+static const char* SENSOR_DOOR_NAME = "door";
+static const uint8_t SENSOR_DOOR_PIN = D1;
+static const unsigned long SENSOR_DOOR_NOTIFY_INTERVAL = 1000; // Every second.
+static const unsigned long SENSOR_DOOR_ALARM_NOTIFY_AFTER = 10000;
 
-#define SENSOR_BOX_NAME "box"
-#define SENSOR_BOX_PIN D2
-#define SENSOR_BOX_NOTIFY_INTERVAL 1000 // Every second.
-#define SENSOR_BOX_ALARM_NOTIFY_AFTER 0
+static const char* SENSOR_BOX_NAME = "box";
+static const uint8_t SENSOR_BOX_PIN = D2;
+static const unsigned long SENSOR_BOX_NOTIFY_INTERVAL = 1000; // Every second.
+static const unsigned long SENSOR_BOX_ALARM_NOTIFY_AFTER = 0;
 
-#define SENSOR_DS18B20_NAME "ds18b20"
-#define SENSOR_DS18B20_PIN D3
-#define SENSOR_DS18B20_PRECISION 12
-#define SENSOR_DS18B20_NOTIFY_INTERVAL 1000 // Every second.
+static const char* SENSOR_DS18B20_NAME = "ds18b20";
+static const uint8_t SENSOR_DS18B20_PIN = D3;
+static const uint8_t SENSOR_DS18B20_PRECISION = 12;
+static const unsigned long SENSOR_DS18B20_NOTIFY_INTERVAL = 1000; // Every second.
 
-#define SENSOR_SCT013_NAME "sct013"
-#define SENSOR_SCT013_PIN A0
-#define SENSOR_SCT013_NOTIFY_INTERVAL 1000 // Every second.
+static const char* SENSOR_SCT013_NAME = "sct013";
+static const uint8_t SENSOR_SCT013_PIN = A0;
+static const unsigned long SENSOR_SCT013_NOTIFY_INTERVAL = 1000; // Every second.
 // http://webmeteobox.ru/docs/SCT013-030V.pdf
-#define SENSOR_SCT013_TURNRATIO 1800.0
-#define SENSOR_SCT013_RESISTANCE 62.0
+static const float SENSOR_SCT013_TURNRATIO = 1800.0;
+static const float SENSOR_SCT013_RESISTANCE = 62.0;
 
-#define ALARM_PIN D4
-#define ALARM_MINIMUM_TIME 2000
+static const uint8_t ALARM_PIN = D4;
+static const unsigned long ALARM_MINIMUM_TIME = 2000;
 
-#define RELE_NAME "fridge"
-#define RELE_PIN D5
+static const char* RELE_NAME = "fridge";
+static const uint8_t RELE_PIN = D5;
 
-#define STATUS_ON "on"
-#define STATUS_OFF "off"
+static const char* STATUS_ON = "on";
+static const char* STATUS_OFF = "off";
